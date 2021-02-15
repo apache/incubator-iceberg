a78b862ad644911b1061d9e38f8f0a3599a251ce
Szehon Ho <szehon_ho@apple.com>
2021-02-15 23:47:44 +0100

    Auto promote sorted column metrics to full
    -- Fix unrelated bug in configuring nested Orc Metrics
    -- Welcome feedback how to avoid propagation of sortOrder through AppenderFactories

--- parquet/src/main/java/org/apache/iceberg/parquet/Parquet.java
+++ parquet/src/main/java/org/apache/iceberg/parquet/Parquet.java
@@ -61,6 +61,7 @@ import org.apache.iceberg.relocated.com.google.common.base.Preconditions;
 import org.apache.iceberg.relocated.com.google.common.collect.Maps;
 import org.apache.iceberg.relocated.com.google.common.collect.Sets;
 import org.apache.iceberg.util.ArrayUtil;
+import org.apache.iceberg.util.SortOrderUtil;
 import org.apache.parquet.HadoopReadOptions;
 import org.apache.parquet.ParquetReadOptions;
 import org.apache.parquet.avro.AvroReadSupport;
@@ -117,7 +118,8 @@ public class Parquet {
     public WriteBuilder forTable(Table table) {
       schema(table.schema());
       setAll(table.properties());
-      metricsConfig(MetricsConfig.fromProperties(table.properties()));
+      metricsConfig(MetricsConfig.fromProperties(table.properties(),
+          SortOrderUtil.getSortedColumns(table.sortOrder())));
       return this;
     }
 
@@ -307,7 +309,8 @@ public class Parquet {
       rowSchema(table.schema());
       withSpec(table.spec());
       setAll(table.properties());
-      metricsConfig(MetricsConfig.fromProperties(table.properties()));
+      metricsConfig(MetricsConfig.fromProperties(
+          table.properties(), SortOrderUtil.getSortedColumns(table.sortOrder())));
       return this;
     }
 
